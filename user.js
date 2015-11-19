a2f478c7a088e9029edbfb1ec2c5ffba56341b04
Gordon Rawe <gordon.tongji@hotmail.com>
2015-11-20 00:46:37 +0800

    something

--- user.js
+++ user.js
@@ -12,7 +12,7 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
     }
 });
 
-var User = sequelize.define('user', {
+var User = sequelize.define('User', {
     firstName: {
         type: Sequelize.STRING,
         field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
@@ -25,6 +25,35 @@ var User = sequelize.define('user', {
     freezeTableName: true // Model tableName will be the same as the model name
 });
 
-User.sync();//put it here which will exec the create table if not exists action.
+var Worker = sequelize.define("Worker", {
+    name: {
+        type: Sequelize.STRING,
+        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    description: {
+        type: Sequelize.STRING,
+        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+    }
+});
+
+var Project = sequelize.define("Project", {
+    name: {
+        type: Sequelize.STRING,
+        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    description: {
+        type: Sequelize.STRING,
+        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+    }
+});
+
+Project.belongsToMany(Worker, {through: 'WorkerProject'});
+Worker.belongsToMany(Project, {through: 'WorkerProject'});
+
+sequelize.sync({force: true}).then(function (status) {
+    console.log("modelling process finished");
+});
 
 module.exports.UserModel = User;
+module.exports.ProjectModel = Project;
+module.exports.WorkerModel = Worker;
