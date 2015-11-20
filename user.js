91627837db751118f31fb613edaed51f33084418
Gordon Rawe <mcluo@ctrip.com>
2015-11-20 17:39:36 +0800

    something

--- user.js
+++ user.js
@@ -4,6 +4,7 @@ var config = require("./config.json");
 
 var sequelize = new Sequelize(config.database, config.username, config.password, {
     host: 'localhost',
+    port: 3307,
     dialect: 'mysql',
     pool: {
         max: 5,
@@ -12,7 +13,7 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
     }
 });
 
-var User = sequelize.define('User', {
+var user = sequelize.define('user', {
     firstName: {
         type: Sequelize.STRING,
         field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
@@ -21,39 +22,89 @@ var User = sequelize.define('User', {
         type: Sequelize.STRING,
         field: 'last_name'
     }
-}, {
-    freezeTableName: true // Model tableName will be the same as the model name
 });
 
-var Worker = sequelize.define("Worker", {
-    name: {
+var image = sequelize.define('image', {
+    url: {
         type: Sequelize.STRING,
-        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+        field: 'url' // Will result in an attribute that is firstName when user facing but first_name in the database
     },
-    description: {
+    type: {
         type: Sequelize.STRING,
-        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+        field: 'type'
     }
 });
 
-var Project = sequelize.define("Project", {
-    name: {
-        type: Sequelize.STRING,
-        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
-    },
-    description: {
-        type: Sequelize.STRING,
-        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
-    }
-});
+user.hasMany(image);
+image.hasOne(user, {foreignKey: "user_image_id", constraints: false});
 
-Project.belongsToMany(Worker, {through: 'WorkerProject'});
-Worker.belongsToMany(Project, {through: 'WorkerProject'});
-
+//
+//var Worker = sequelize.define("Worker", {
+//    name: {
+//        type: Sequelize.STRING,
+//        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    },
+//    description: {
+//        type: Sequelize.STRING,
+//        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    }
+//});
+//
+//var Project = sequelize.define("Project", {
+//    name: {
+//        type: Sequelize.STRING,
+//        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    },
+//    description: {
+//        type: Sequelize.STRING,
+//        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    }
+//});
+//
+//Project.belongsToMany(Worker, {through: 'WorkerProject'});
+//Worker.belongsToMany(Project, {through: 'WorkerProject'});
+//
+//var teacher = sequelize.define("teacher", {
+//    name: {
+//        type: Sequelize.STRING,
+//        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    },
+//    description: {
+//        type: Sequelize.STRING,
+//        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    }
+//});
+//
+//var course = sequelize.define("course", {
+//    name: {
+//        type: Sequelize.STRING,
+//        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    },
+//    description: {
+//        type: Sequelize.STRING,
+//        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    }
+//});
+//
+//var student = sequelize.define("student", {
+//    name: {
+//        type: Sequelize.STRING,
+//        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    },
+//    description: {
+//        type: Sequelize.STRING,
+//        field: 'description' // Will result in an attribute that is firstName when user facing but first_name in the database
+//    }
+//});
+//
+//teacher.belongsToMany(course, {through: "teachers_courses"});
+//course.belongsToMany(teacher, {through: "teachers_courses"});
+//student.belongsToMany(course, {through: "courses_students"});
+//course.belongsToMany(student, {through: "courses_students"});
+//
 sequelize.sync({force: true}).then(function (status) {
     console.log("modelling process finished");
 });
 
-module.exports.UserModel = User;
-module.exports.ProjectModel = Project;
-module.exports.WorkerModel = Worker;
+module.exports.UserModel = user;
+module.exports.ImageModel = image;
